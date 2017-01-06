f17a60cafac4552d95c529fe2de14abd12da9cb2
fedorenkokhalilov@gmail.com <Ra11235813213>
2017-01-06 11:31:11 +0200

    Exemple animation

---
+++ Assets/RobotController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotController : MonoBehaviour {
+	[SerializeField]
+	private Animator _animator;
+	private bool _isWalk;
+	[SerializeField,Range(0,1)]
+	private float _speed;
+	[SerializeField]
+	private SpriteRenderer _sprite;
+	
+	void Update () {
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			_animator.SetBool("Walk",true);
+			_sprite.flipX = false;
+			_animator.transform.Translate (new Vector3(1,0)*_speed);
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			_animator.SetBool("Walk",true);
+			_sprite.flipX = true;
+			_animator.transform.Translate (new Vector3(-1,0)*_speed);
+		}
+
+		if (Input.GetKeyUp (KeyCode.RightArrow)) 
+		{
+			_animator.SetBool("Walk",false);
+		}
+
+		if (Input.GetKeyUp (KeyCode.LeftArrow))
+		{
+			_animator.SetBool("Walk",false);
+		}
+	}
+}
---
+++ Assets/SphereAnimController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereAnimController : MonoBehaviour {
+	[SerializeField]
+	private Animator _animator;
+
+	[SerializeField]
+	KeyCode _key;
+
+	private bool _isAnimOn;
+
+	public void AnimationEvent1()
+	{
+		print ("Somethin");
+	}
+
+	void OnGUI()
+	{
+		//if (Input.GetKeyDown(KeyCode.Space)) {
+			//_isAnimOn = !_isAnimOn;
+		//	_animator.SetBool("UpDownStart", _isAnimOn);
+		//}
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(_key)) {
+			//_isAnimOn = !_isAnimOn;
+			_animator.SetBool("UpDownStart",!_animator.GetBool("UpDownStart"));
+		}
+	}
+}
